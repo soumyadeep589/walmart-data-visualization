0e86a2856f55af78dc8d9d9b22243ca7204366d1
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-03-11 15:55:52 +0530

    add db in settings.py

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "walmart",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
