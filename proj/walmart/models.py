fee3fb090986c2c4dbc941c79d64df58a5bcd5b3
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-03-12 13:35:33 +0530

    add product model, basic store to db code

--- proj/walmart/models.py
+++ proj/walmart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=250)
+    previous_price = models.DecimalField(max_digits=10, decimal_places=4)
+    display_price = models.DecimalField(max_digits=10, decimal_places=4)
+    is_out_of_stock = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+        
+    class Meta:
+        db_table = "product"
+        verbose_name_plural = "products"
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}, product_id: {self.product_id}, name: {self.name}"
---
+++ proj/walmart/scripts/store.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import django
+import requests
+import json
+import random
+
+proj_dir = os.environ["PROJDIR"]
+sys.path.append(os.path.join(proj_dir, "dj"))
+os.environ["DJANGO_SETTINGS_MODULE"] = "proj.settings"
+django.setup()
+
+from scraper_api import ScraperAPIClient
+from walmart.models import Product
+from walmart.scripts.product_keys import product_keys
+from walmart.scripts.upload_constants import base_url, count, offset, page, store_id
+
+
+def store_product_info(product_keys):
+    for key in product_keys:
+        url = gen_url(base_url, count, offset, page, store_id, key)
+        client = ScraperAPIClient('2afffa25a502bf4f254f972578ad9550')
+        result = client.get(url=url).json()
+        print(result["products"][0]["USItemId"])
+        store_to_db(result["products"])
+
+
+def store_to_db(products):
+    for product in products:
+        product_obj = Product.objects.create(
+            product_id=product["USItemId"],
+            name=product["basic"]["name"],
+            previous_price=product["store"]["price"]["previousPrice"],
+            display_price=product["store"]["price"]["displayPrice"],
+            is_out_of_stock=product["store"]["isOutOfStock"]
+        )
+        product_obj.save()
+
+
+def gen_url(base_url, count, offset, page, store_id, query):
+    url = f"{base_url}?count={count}&offset={offset}&page={page}&storeId={store_id}&query={query}"
+    return url
+
+if __name__ == "__main__":
+    store_product_info(product_keys)
