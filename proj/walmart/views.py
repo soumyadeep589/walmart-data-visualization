f98e34e3c4b172179c3b1f6833e9f2711bc225b8
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-03-17 18:38:27 +0530

    add csrf_exempt decorator

--- proj/walmart/views.py
+++ proj/walmart/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from walmart.models import Product
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+
+@csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
