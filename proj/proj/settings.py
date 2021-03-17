06fef806057cd7be2475a78b206fac038e27ee58
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-03-17 10:20:55 +0530

    frontend initial

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -32,7 +32,7 @@ SCRAPER_API_KEY = os.environ.get('SCRAPER_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
