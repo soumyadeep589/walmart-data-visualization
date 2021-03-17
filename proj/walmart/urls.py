f1b65ecb8fdb669ade7390b410f01c1b35a5f557
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-03-17 18:20:42 +0530

    add basic graph for a static product

--- proj/walmart/urls.py
+++ proj/walmart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^api/chart/data/$', views.ChartData.as_view(), name='api-data'),
 ]
 
--- proj/walmart/views.py
+++ proj/walmart/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from walmart.models import Product
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+class ChartData(APIView):
+
+    def get(self, request, format=None):
+        products = Product.objects.filter(product_id=489882644).values()
+        name=products[0]["name"]
+        dates = [elem["created_on"].date() for elem in products]
+        prices = [elem["display_price"] for elem in products]
+        data = {
+                "name": name,
+                "dates": dates,
+                "prices": prices,
+        }
+        return Response(data)
+
+    
