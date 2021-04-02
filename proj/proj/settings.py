6f810c408de133146fd0f515d2380e73c0fdac9a
Soumyadeep Mondal <soumyadeep589@gmail.com>
2021-04-02 15:11:59 +0530

    move email host pass to .env

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -214,8 +214,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'alexx.bang992@gmail.com'
-EMAIL_HOST_PASSWORD = 'alex@101'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'home'
